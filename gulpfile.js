0c84df1c34776b243b206f9316031afc0b761596
John Deck <jdeck88@gmail.com>
2024-06-26 11:51:42 -0400

    updating app and gulpfile to point to revamped biscicol-server service

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,47 @@
-var gulp = require('gulp');
-var rimraf = require('rimraf');
+const gulp = require('gulp');
+const fs = require('fs');
+const path = require('path');
+const { series, src, dest } = gulp;
 
+// Custom clean function using fs and path
+function clean(cb) {
+    const directory = './public';
 
-function defaultTask(cb) {
-    
-    cb()
-    gulp.src('app/*')    
-        .pipe(gulp.dest('public/'))
-    gulp.src('app/trait-viz/lib/*')    
-        .pipe(gulp.dest('public/trait-viz/lib/'))
+    // Check if directory exists
+    if (fs.existsSync(directory)) {
+        // Recursively remove directory
+        removeDirectory(directory);
+    }
+
+    cb(); // Callback to indicate completion
 }
-  
-exports.default = defaultTask
 
-gulp.task('clean', function(cb) {
-    rimraf('./public', cb);
-});
+// Recursive function to remove directory
+function removeDirectory(directory) {
+    fs.readdirSync(directory).forEach((file) => {
+        const filePath = path.join(directory, file);
+        if (fs.lstatSync(filePath).isDirectory()) {
+            removeDirectory(filePath);
+        } else {
+            fs.unlinkSync(filePath);
+        }
+    });
+    fs.rmdirSync(directory);
+}
+
+// Example of other tasks
+function defaultTask(cb) {
+    src('app/*')
+        .pipe(dest('public/'))
+        .on('end', function() {
+            src('app/trait-viz/lib/*')
+                .pipe(dest('public/trait-viz/lib/'))
+                .on('end', cb);
+        });
+}
+
+// Register tasks
+exports.clean = clean;
+exports.default = defaultTask;
+exports.build = series(clean, defaultTask);
+
